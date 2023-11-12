4e2cea47f2f4ccae7d26a52e315a7d113e9cbe45
Nguyễn Ngọc Thông <thongnguyenngoc3738@gmail.com>
2023-11-12 16:22:17 +0700

    Initial commit

---
+++ src/main/java/com/supplierBHX/SupplierBhxApplication.java
@@ -0,0 +1,14 @@
+package com.supplierBHX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierBhxApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(SupplierBhxApplication.class, args);
+	}
+
+}
